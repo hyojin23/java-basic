5e721e1710a9636f839c70f1f941cf8f974b4932
hyojin23 <kimhj9292@gmail.com>
2024-11-11 18:03:30 +0900

    섹션8. 정리까지

--- src/static2/ex/MathArrayUtils.java
+++ src/static2/ex/MathArrayUtils.java
@@ -3,18 +3,38 @@ package static2.ex;
 public class MathArrayUtils {
 
     private MathArrayUtils() {
-
+        //private 인스턴스 생성을 막는다.
     }
 
     public static int sum(int[] values) {
-        int sum = 0;
-        for (int num : values) {
-            sum += num;
+        int total = 0;
+        for (int value : values) {
+            total += value;
         }
-        return sum;
+        return total;
     }
 
-    public static int average(int[] values) {
-        return sum(values) / values.length;
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int minValue = values[0];
+        for (int value : values) {
+            if (value < minValue) {
+                minValue = value;
+            }
+        }
+        return minValue;
+    }
+
+    public static int max(int[] values) {
+        int maxValue = values[0];
+        for (int value : values) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
     }
 }
